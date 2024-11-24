1662a6a9deb7fb034122685b3f8b576196f2949e
Julian Diaz <julian.diaz@dev-codes.io>
2024-11-24 17:01:12 -0500

    -FIX send notification component layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,6 @@ import { PersonaService } from './service/persona.service';
 import { HeaderV2Component } from './components/header-v2/header-v2.component';
 import { FooterV2Component } from './components/footer-v2/footer-v2.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatButtonModule} from '@angular/material/button';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
@@ -40,8 +38,6 @@ import { HomeComponent } from './components/home/home.component';
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatTooltipModule,
-    MatButtonModule
   ],
   providers: [AuthService, PersonaService],
   bootstrap: [AppComponent]
--- src/app/feature/inbox/components/inbox/inbox.component.ts
+++ src/app/feature/inbox/components/inbox/inbox.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inbox',
   templateUrl: './inbox.component.html',
-  styleUrls: ['./inbox.component.scss']
+  styleUrls: ['./inbox.component.scss'],
 })
 export class InboxComponent {
+
+  constructor(private router: Router) {}
+
+  navigateToSendNotification(): void {
+    if (localStorage.getItem('persona')) {
+      this.router.navigate(['/send-notification']);
+    } else {
+      alert('Debe iniciar sesión o registrarse primero.');
+      this.router.navigate(['/login']);
+    }
+  }
 
 }

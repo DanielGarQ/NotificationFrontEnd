af28f06d7acf1efef74ed928d114d93967021f9f
Julian Diaz <julian.diaz@dev-codes.io>
2024-11-24 01:01:51 -0500

    -FIX login layout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './feature/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './feature/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 // import { HomeComponent } from './feature/home/home.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './shared/shared.module';
-import { LoginComponent } from './feature/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './feature/register/register.component';
 import { SendNotificationModule } from './feature/send-notification/send-notification.module';
 import { InboxModule } from './feature/inbox/inbox.module';
